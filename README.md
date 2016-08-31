130b71c95da18a8e841d496656a1895e0493dcb5
Dani Adkins <danirenaeadkins12@gmail.com>
2016-08-31 18:39:50 -0500

    completed exercise

---
+++ README.md
@@ -0,0 +1,22 @@
+# Chaining Exercise
+
+## Description
+
+This exercise outputs in the console as follows:<br>
+  1. numbers from the integers array in descending order<br>
+  2. numbers from the integers array that are lesser than 19<br>
+  3. the remaining numbers from the above function multiplied by 1.5 and subtracted by 1<br>
+  4. the sum of the remaining numbers from the above three functions<br>
+
+
+## How to run
+
+In your terminal run:
+
+```
+http-server -p 9999
+```
+then navigate to http://localhost:9999 in your browser.
+
+## Contributors
+- [Dani Adkins](https://github.com/itsdanirenae)
---
+++ chaining.js
@@ -0,0 +1,47 @@
+
+var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
+
+// sort the numbers in descending order
+
+function integerSorter (a, b) {
+  return a - b;
+}
+
+var sortedIntegers = integers.sort(integerSorter);
+console.log(sortedIntegers);
+
+// Remove any integers greater than 19
+
+function integerValue(myIntegers){
+  return (myIntegers <= 19);
+}
+
+var filteredIntegers = integers.filter(integerValue);
+console.log(filteredIntegers);
+
+
+// Multiply each remaining number by 1.5 and then subtract 1
+
+function multipliedIntegers(a,b){
+  return a + b;
+}
+
+var multipliedIntegerz = filteredIntegers.reduce(multipliedIntegers);
+var multipliedIntegerz2 = multipliedIntegerz * 1.5 - 1;
+console.log(multipliedIntegerz2);
+
+
+// Then output (either in the DOM or the console) the sum of all the resulting numbers
+
+
+function sum (a,b){
+  return a + b;
+}
+
+var sumz = 0
+  sumz += filteredIntegers.reduce(sum);
+  sumz += sortedIntegers.reduce(sum);
+  sumz += multipliedIntegerz2;
+
+console.log(sumz);
+document.write("final output: ", sumz);
